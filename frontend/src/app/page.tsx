1287b390136ba444e6811f7a665676f34b6951aa
Sandesh Shrestha <sandes.sth@gmail.com>
2023-08-02 02:52:38 -0700

    create UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import Motto from "./components/layout/motto";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Motto />
+    </div>
+  );
 }
 
 export default Home;
