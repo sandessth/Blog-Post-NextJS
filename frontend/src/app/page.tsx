e04ef2fb4fb514ea05fa7e9234674612765ed00a
Sandesh Shrestha <sandes.sth@gmail.com>
2023-08-07 12:31:39 -0700

    save blog data in context and access blogcontext

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,18 @@ import React from "react";
 // import Image from "next/image";
 import Motto from "./components/layout/motto";
 import Display from "./components/layout/display-blog";
-import Display1 from "./components/layout/display-from-CMS";
+// import Display1 from "./components/layout/display-from-CMS";
+import Header from "./components/layout/header";
+// import Footer from "./components/layout/footer";
 // import SideNav from "./components/layout/sidenav";
 
 function Home() {
   return (
     <div>
+      {/* <Header /> */}
       <Motto />
       <Display />
-      <Display1 />
+      {/* <Display1 /> */}
     </div>
   );
 }
